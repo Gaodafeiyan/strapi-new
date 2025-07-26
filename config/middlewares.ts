79bcb6c621dd4f4aa7fe7e4627c2865721e26fb9
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-27 01:25:03 +0800

    修复中间件名称：使用strapi::jwt和strapi::permissions

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,6 +9,6 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  'plugin::users-permissions.jwt',
-  'plugin::users-permissions.permissions',
+  'strapi::jwt',
+  'strapi::permissions',
 ];
