a5e56bd539215a08a6d7ac11b15423c48355b8f4
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-26 23:30:49 +0800

    修复users-permissions插件配置，添加默认角色设置

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default () => ({
+  'users-permissions': {
+    config: {
+      jwt: { expiresIn: '7d' },
+      register: {
+        defaultRole: 'authenticated'  // 关键：设置默认角色
+      }
+    }
+  }
+});
